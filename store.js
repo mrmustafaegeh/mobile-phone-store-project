7c558f02fe81168233d51aab6ca0f1b719b209ea
mustafa egeh <Mr.mustafaegeh@gmail.com>
2025-05-05 17:45:34 +0300

    init commit

---
+++ store.js
@@ -0,0 +1,7 @@
+function addToCart(name, price) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    cart.push({ name, price });
+    localStorage.setItem('cart', JSON.stringify(cart));
+    alert(`${name} added to cart!`);
+  }
+
